cdd00caddd727f0d3a1fced320c520c1088d13fa
Hugo Pedder <hp17602@bristol.ac.uk>
2019-05-03 17:36:12 +0100

    rank.MBNMA completed

--- R/rank.functions.R
+++ R/rank.functions.R
@@ -13,18 +13,30 @@ rank.MBNMA.predict <- function() {
 #' Rank parameter estimates
 #'
 #' Only parameters that vary by agent can be ranked
-rank.MBNMA <- function(mbnma, params=NULL, direction=1, agents=NULL) {
+#'
+#' @param to.rank A numeric vector containing the codes for the agents you wish to rank.
+#' If left `NULL` then all agents in the model will be ranked.
+rank.MBNMA <- function(mbnma, params=NULL, direction=1, to.rank=NULL, level="agent") {
 
   # Checks
   argcheck <- checkmate::makeAssertCollection()
   checkmate::assertClass(mbnma, classes="MBNMA", add=argcheck)
-  checkmate::assertChoice(params, choices = , null.ok=TRUE, add=argcheck)
+  checkmate::assertCharacter(params, null.ok=TRUE, add=argcheck)
   checkmate::assertChoice(direction, choices = c(-1,1), add=argcheck)
+  checkmate::assertNumeric(to.rank, lower = 2, null.ok=TRUE, add=argcheck)
+  checkmate::assertChoice(level, choices = c("agent","class"), add=argcheck)
   checkmate::reportAssertions(argcheck)
 
+  if (level=="class") {
+    if (is.null(mbnma[["model"]][["data"]]()[["class"]])) {
+      stop("`level` has been set to `class` but classes have not been used in the model")
+    }
+  }
+
   # If treats have not been specified then select all of them - WONT WORK IF PLACEBO NOT INCLUDED
-  if (is.null(agents)) {
-    agents <- c(2:mbnma[["model"]][["data"]]()[["agent"]])
+  codes.mod <- c(2:max(mbnma[["model"]][["data"]]()[[level]], na.rm=TRUE))
+  if (is.null(to.rank)) {
+    to.rank <- codes.mod
   }
 
   if (direction==-1) {
@@ -33,6 +45,13 @@ rank.MBNMA <- function(mbnma, params=NULL, direction=1, agents=NULL) {
     decreasing <- TRUE
   } else {stop("`direction` must be either -1 or 1 for ranking")}
 
+  if (is.null(params)) {
+    for (i in seq_along(mbnma[["parameters.to.save"]])) {
+      if (length(mbnma$BUGSoutput$long.short[i][[1]])==length(codes.mod)) {
+        params <- append(params, mbnma[["parameters.to.save"]][i])
+      }
+    }
+  }
 
   rank.result <- list()
   for (i in seq_along(params)) {
@@ -40,22 +59,72 @@ rank.MBNMA <- function(mbnma, params=NULL, direction=1, agents=NULL) {
       param.mod <- mbnma[["BUGSoutput"]][["sims.list"]][[params[i]]]
 
       # Check that selected parameter is different over multiple treatments
-      if (!is.matrix(param.mod) | ncol(param.mod)<=1) {
-        msg <- paste0(params[i], " does not vary by treatment and therefore cannot be ranked by treatment")
+      if (!is.matrix(param.mod) | ncol(param.mod)!=length(codes.mod)) {
+        msg <- paste0(params[i], " does not vary by ", level, " and therefore cannot be ranked")
         stop(msg)
       }
 
-      param.mod <- param.mod[,treats]
+      param.mod <- param.mod[,to.rank-1]
       rank.mat <- t(apply(param.mod, MARGIN=1, FUN=function(x) {
         order(order(x, decreasing = decreasing), decreasing=FALSE)
       }))
-      colnames(rank.mat) <- treats
+      colnames(rank.mat) <- to.rank
 
       rank.result[[params[i]]] <-
         list("summary"=sumrank(rank.mat),
-             "prob.matrix"=calcprob(rank.mat, treats=treats),
+             "prob.matrix"=calcprob(rank.mat, treats=to.rank),
              "rank.matrix"=rank.mat)
 
     }
   }
+  return(rank.result)
+}
+
+
+
+
+
+#' Calculates a matrix of ranking probabilities from a matrix of treatment/agent/class
+#' rankings
+calcprob <- function(rank.mat, treats=NULL) {
+  NT <- ncol(rank.mat)
+  rank.prob <- vector(length=NT)
+
+  for (c in 1:NT) {
+    pos.vec <- vector()
+    for (r in 1:NT) {
+      pos.vec <- append(pos.vec,
+                        length(rank.mat[rank.mat[,c]==r,c])/nrow(rank.mat))
+    }
+    rank.prob <- cbind(rank.prob, pos.vec)
+  }
+  rank.prob <- rank.prob[,-1]
+
+  if (!is.null(treats)) {
+    colnames(rank.prob) <- treats
+  }
+
+  return("rank.prob"=rank.prob)
+}
+
+
+
+
+
+#' Generates a summary data frame from a matrix of treatment/agent/class rankings
+sumrank <- function(rank.mat) {
+  if (is.null(colnames(rank.mat))) {
+    colnames(rank.mat) <- c(1:ncol(rank.mat))
+  }
+
+  quantiles.rank <- apply(X=rank.mat, MARGIN = 2,
+                          function(x) quantile(x, probs=c(0.025, 0.25, 0.5, 0.75, 0.975)))
+  summary.rank <- data.frame(
+    "treatment"=colnames(rank.mat),
+    "mean"= apply(X=rank.mat, MARGIN = 2, mean),
+    "sd"= apply(X=rank.mat, MARGIN = 2, sd)
+  )
+  summary.rank <- cbind(summary.rank, t(quantiles.rank))
+
+  return(summary.rank)
 }
