9ac1d41da9545ab5cafbe6d13f1e37139cd5f074
Hugo <hugopedder@gmail.com>
2020-02-13 13:33:21 +0000

    cutjags only called if mbnma.jags runs properly

--- R/run.functions.R
+++ R/run.functions.R
@@ -503,7 +503,9 @@ mbnma.run <- function(network,
   }
 
   # Remove dose-response parameters for agents if multi dr functions are used
-  result <- cutjags(result)
+  if (!is.list(result)) {
+    result <- cutjags(result)
+  }
 
   if (!("error" %in% names(result))) {
     class(result) <- c("mbnma", class(result))
