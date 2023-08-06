06b4dfb08143fe7c7c08d0c65050d7120565b0e7
Hugo <hugo.pedder@bristol.ac.uk>
2023-08-06 19:10:30 +0100

    added new globals

--- R/globals.R
+++ R/globals.R
@@ -4,7 +4,7 @@
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "studyID", "agent", "dose", "Var1", "value",
-                                                        "Parameter", "fupdose", "groupvar", "y",
+                                                        "Parameter", "fupdose", "groupvar", "y", "sd", "stansd",
                                                         "network", "a", "param", "med", "l95", "u95", "value",
                                                         "Estimate", "2.5%", "50%", "97.5%", "treatment",
                                                         "study", "arm", "mod1.mean", "mod2.mean", "argcheck",
